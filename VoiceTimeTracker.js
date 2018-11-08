f7fb11aa9c3d96285730093b63dbef6ad1ca56b9
Kevin Lucas <ktlucas03@gmail.com>
2018-11-07 20:54:50 -0600

    bot now pull sall users in voice and adds them to a JSON for tracking the time'

--- VoiceTimeTracker.js
+++ VoiceTimeTracker.js
@@ -1,9 +1,11 @@
-//change this for git learning
-
 const request = require('request');
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+var oldUsers = [];  //used to look at the past users in voice channel. This will tell us if they stayed or are new. 
+var newUsers = [];
+var jsonTest = require('./JSONtest.json');
+var fs = require('fs');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -29,21 +31,54 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	{
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+		
+
         switch(cmd) 
 		{
-            // !ping
             case 'who':
-			var id;
-			const channels = bot.channels;
-			for(var c in channels)//each channel id in the server
-			{ 		
-				mems = bot.channels[c].members;   //each member in that channel
-				for(var m in mems)
+
+			const channels = bot.channels;//ALL channels the bot is linked to. 
+			for(var cid in channels)//each channel id in the server
+			{ 	
+				var mems = bot.channels[cid].members;   //each member collection in that channel
+				for(var mid in mems)
 				{ 		
-					logger.info(m + '-' + bot.users[m].username);      
+					newUsers.push(mid);  //array of user IDS
+					for(var old in oldUsers)
+					{
+						if(mid == oldUsers[old])  //we need to ++TIME
+						{
+							//FIND in JSON
+							logger.info(bot.users[mid].username + ' ' + mid + ' - found in old ++Time'); 
+							var inJSON = 0;
+							for(var l=0; l<jsonTest.userList.length; l++)
+							{
+								if(mid == jsonTest.userList[l].ID)  //found in the JSON
+								{
+									inJSON = 1; //found in JSON
+									jsonTest.userList[l].Name = bot.users[mid].username; //update name if it changes
+									jsonTest.userList[l].Time++;
+									logger.info(bot.users[mid].username + ' ' + jsonTest.userList[l].Time);
+								}
+							}
+							//need a not found then add to JSON
+							if(inJSON == 0)
+							{
+								var pushUser = {"ID":mid,"Name":bot.users[mid].username,"Time":1,"Messages":0};
+								jsonTest.userList.push(pushUser);
+							}
+
+						}
+					}
+
 				}
+				
 			}
+			oldUsers = [];
+			oldUsers = newUsers.slice();
+			newUsers = [];
+			fs.writeFile('./JSONtest.json', JSON.stringify(jsonTest), function (err) {if (err) return console.log(err);}); //write to JSON
+			
             break;
             // Just add any case commands if you want to..
          }
